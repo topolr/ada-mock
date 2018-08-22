af32d523f5adb40168fcb32d6c88e60e054f6778
wangjinliang <wangjinliang@huobi.com>
2018-08-22 18:23:49 +0800

    pass

---
+++ new/test.js
@@ -0,0 +1,6 @@
+let File=require("./lib/file");
+let Path=require("path");
+let yaml=require("yamljs");
+
+let result=yaml.parse(new File(Path.resolve(__dirname,"./test.yaml")).readSync());
+console.log(result);
