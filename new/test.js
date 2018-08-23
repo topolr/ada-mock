6a45950e599b754685663a7a1697444e4b44a264
wangjinliang <wangjinliang@huobi.com>
2018-08-23 17:48:49 +0800

    pass

--- new/test.js
+++ new/test.js
@@ -3,4 +3,4 @@ let Path=require("path");
 let yaml=require("yamljs");
 
 let result=yaml.parse(new File(Path.resolve(__dirname,"./test.yaml")).readSync());
-console.log(result);
+console.log(JSON.stringify(result,null,4));
