aea830581ee791d616f26f35fb6b46194aef76cb
wangjinliang <wangjinliang@huobi.com>
2018-08-24 18:19:14 +0800

    pass

--- base/parser.js
+++ base/parser.js
@@ -21,43 +21,70 @@ let Parser = {
 			Reflect.ownKeys(data).forEach(key => {
 				let a = key.split("<%|%>");
 				data[a[0]] = {
-					__comment__: a[1]?a[1].replace(/<%\|\|%>/g, os.EOL):"",
+					__comment__: a[1] ? a[1].replace(/<%\|\|%>/g, os.EOL) : "",
 					__props__: this.parseData(data[key])
 				};
 				delete data[key];
 			});
+		} else {
+			if (data.indexOf("<%|%>") !== -1) {
+				let t = data.trim().split("<%|%>");
+				return {
+					__value__: t[0].trim(),
+					__comment__: t[1]
+				}
+			}
 		}
 		return data;
 	},
 	setFinalInfo(data) {
 		if (Array.isArray(data)) {
-			data.forEach((item, index) => {
+			data.forEach((item) => {
 				this.setFinalInfo(item);
 			});
 		} else if (util.isObject(data)) {
-			Reflect.ownKeys(data).forEach(key => {
-				if (data[key].__props__ && util.isString(data[key].__props__)) {
-					let k = data[key].__props__, t = k.match(/([a-zA-Z_0-9$]+)\((.*)?\)/);
-					let typeName = "", parameter = "";
-					if (t) {
-						typeName = t[1];
-						parameter = t[2];
-					} else {
-						typeName = data[key].__props__.trim();
-					}
-					let typeInfo = Types.getType(typeName);
-					if (typeInfo) {
-						data[key] = Object.assign({
-							__comment__: data[key].__comment__,
-							parameter
-						}, typeInfo);
-					} else {
-						throw Error(`can not find type name of ${typeName}`);
-					}
+			if (data.__value__) {
+				let k = data.__value__, t = k.match(/([a-zA-Z_0-9$]+)\((.*)?\)/);
+				let typeName = "", parameter = "";
+				if (t) {
+					typeName = t[1];
+					parameter = t[2];
 				} else {
-					this.setFinalInfo(data[key].__props__);
+					typeName = data.__value__.trim();
 				}
-			});
+				let typeInfo = Types.getType(typeName);
+				if (typeInfo) {
+					Object.assign(data,{
+						parameter
+					}, typeInfo);
+				} else {
+					throw Error(`can not find type name of ${typeName}`);
+				}
+			} else {
+				Reflect.ownKeys(data).forEach(key => {
+					if (data[key].__props__ && util.isString(data[key].__props__)) {
+						let k = data[key].__props__, t = k.match(/([a-zA-Z_0-9$]+)\((.*)?\)/);
+						let typeName = "", parameter = "";
+						if (t) {
+							typeName = t[1];
+							parameter = t[2];
+						} else {
+							typeName = data[key].__props__.trim();
+						}
+						let typeInfo = Types.getType(typeName);
+						if (typeInfo) {
+							data[key] = Object.assign({
+								__comment__: data[key].__comment__,
+								parameter
+							}, typeInfo);
+						} else {
+							throw Error(`can not find type name of ${typeName}`);
+						}
+					} else {
+						this.setFinalInfo(data[key].__props__);
+					}
+				});
+			}
 		}
 	},
 	parseBaseInfo(content) {
